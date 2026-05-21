b1b2b53b180c187763036c8bfed2027e21ad68fd
shivang-jnv <skjnvspn@gmail.com>
2026-05-21 20:16:03 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t shivangjnv/java-maven-app:2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push shivangjnv/java-maven-app:2.0'
