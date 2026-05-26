0c62bbc112a33bcf4b2a7e8bbcfc020a71d5f0ad
shivang-jnv <skjnvspn@gmail.com>
2026-05-27 00:12:49 +0530

    fixed github url typo

--- script.groovy
+++ script.groovy
@@ -35,7 +35,7 @@ def commitVersion(){
         sh 'git branch'
         sh 'git config --list'
 
-        sh "git remote set-url origin https://${USER}:{PASS}@github.com/shivang-jnv/java-maven-app.git"
+        sh "git remote set-url origin https://${USER}:${PASS}@github.com/shivang-jnv/java-maven-app.git"
         sh 'git add .'
         sh 'git commit -m "ci: version bump"'
         sh 'git push origin HEAD:jenkins-jobs'
